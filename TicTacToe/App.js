bac7cae953cbbb1a5dc3ee481a65ee4a9abcfccf
@God-Speed <91541763+abhinav-2403@users.noreply.github.com>
2023-04-15 20:08:14 +0530

    Add files via upload

---
+++ TicTacToe/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Board from './TicTacToeGame/Board';
+
+function App () {
+
+  return (
+    <div className="App">
+        <Board/>
+    </div>
+  );
+}
+
+export default App;
